76076989402dcf4099a0c1e17d402b6809114972
Akuma <aeres99@hotmail.com>
2021-06-28 15:33:50 +0000

    sloppy updated

--- sloppy.sh
+++ sloppy.sh
@@ -1,1 +1,15 @@
 #!/bin/env bash
+
+
+#
+# A simple script that utilises tesseract ocr engine to basically
+# get a text location selection area and then moves it to clipboard, super nice
+#
+imagefile="/tmp/sloppy.$RANDOM.png"
+text="/tmp/translation"
+echo "$imagefile"
+slop=$(slop -f "%g") || exit 1
+read -r G < <(echo $slop)
+import -window root -crop $G $imagefile
+tesseract $imagefile $text 2>/dev/null 
+cat $text".txt" | xclip -selection c
